dbc13f30fff63540756463ecd9cdcb3216c17236
zest-php <contact@zest-php.com>
2011-01-11 07:49:56 +0100

    surcharge de la méthode setName sur l'élément de formulaire file

--- library/Zest/Form/Element/File.php
+++ library/Zest/Form/Element/File.php
@@ -8,6 +8,40 @@
 class Zest_Form_Element_File extends Zend_Form_Element_File{
 	
 	/**
+	 * @param string $type
+	 * @return Zend_Loader_PluginLoader_Interface
+	 */
+	public function getPluginLoader($type){
+		$type = strtoupper($type);
+		if($type == self::TRANSFER_ADAPTER){
+			$exists = array_key_exists($type, $this->_loaders);
+			$loader = parent::getPluginLoader($type);
+			if(!$exists){
+				$loader->addPrefixPath('Zest_File_Transfer_Adapter', 'Zest/File/Transfer/Adapter/');
+			}
+			return $loader;
+		}
+		return parent::getPluginLoader($type);
+	}
+	
+	/**
+	 * @param string $name
+	 * @return Zest_Form_Element_File
+	 */
+	public function setName($name){
+		if($this->_adapter){
+			$destination = $this->getDestination();
+			$this->_adapter = null;
+			parent::setName($name);
+			$this->setDestination($destination);
+		}
+		else{
+			parent::setName($name);
+		}
+		return $this;
+	}
+	
+	/**
 	 * @param Zend_View_Interface $view
 	 * @return string
 	 */
