db4f7b3712db1b72ba4e8927c7df06fffaaaed31
zest-php <contact@zest-php.com>
2011-02-12 09:06:08 +0100

    correction de la ressource d'application view si le helper viewRenderer existe déjà

--- libraries/Zest/Application/Resource/View.php
+++ libraries/Zest/Application/Resource/View.php
@@ -16,11 +16,8 @@ class Zest_Application_Resource_View extends Zend_Application_Resource_ResourceA
 	 * @return void
 	 */
 	public function init(){
-		$view = $this->getView();
-		
-		$viewRenderer = new Zest_Controller_Action_Helper_ViewRenderer();
-		$viewRenderer->setView($view);
-		Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+		$this->getView();
+		$this->_initView();
 	}
 	
 	/**
@@ -28,23 +25,43 @@ class Zest_Application_Resource_View extends Zend_Application_Resource_ResourceA
 	 */
 	public function getView(){
 		if(is_null($this->_view)){
-			$this->_view = new Zest_View();
-			
-			$options = $this->getOptions();
-			$engine = isset($options['engine']) ? strtolower($options['engine']) : null;
-			if($engine == 'zend'){
-				unset($options['engine']);
+			if(Zend_Controller_Action_HelperBroker::hasHelper('viewRenderer')){
+				$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
 			}
 			else{
-				if($engine && isset($options[$engine])){
-					$options = array_merge($options, $options[$engine]);
-					unset($options[$engine]);
-				}
+				$viewRenderer = new Zest_Controller_Action_Helper_ViewRenderer();
+				Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
 			}
 			
-			$this->_view->setOptions($options);
+			if($viewRenderer->view){
+				$this->_view = $viewRenderer->view;
+			}
+			else{
+				$this->_view = new Zest_View();
+				$viewRenderer->setView($this->_view);
+			}
 		}
 		return $this->_view;
+	}
+	
+	/**
+	 * @return void
+	 */
+	protected function _initView(){
+		$options = $this->getOptions();
+		$engine = isset($options['engine']) ? strtolower($options['engine']) : null;
+		
+		if($engine == 'zend'){
+			unset($options['engine']);
+		}
+		else{
+			if($engine && isset($options[$engine])){
+				$options = array_merge($options, $options[$engine]);
+				unset($options[$engine]);
+			}
+		}
+		
+		$this->_view->setOptions($options);
 	}
 	
 	
