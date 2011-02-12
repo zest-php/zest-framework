b7c4c3c6685ae058d4ffa4ef83b46a3747377daa
zest-php <contact@zest-php.com>
2011-02-12 08:53:51 +0100

    nouvelle acl et correction ticket #3

--- libraries/Zest/Acl.php
+++ libraries/Zest/Acl.php
@@ -42,16 +42,16 @@ class Zest_Acl extends Zend_Acl{
 	/**
 	 * @param Zend_Acl_Resource_Interface|string $resource
 	 * @param Zend_Acl_Resource_Interface|string $parent
-	 * @param string $idProperty
+	 * @param array $options
 	 * @return Zest_Acl
 	 */
-	public function addResource($resource, $parent = null, $idProperty = null){
-		if($resource instanceof Zend_Acl_Resource_Interface){
-			$resource = $resource->getResourceId();
+	public function addResource($resource, $parent = null, array $options = array()){
+		if(is_string($resource)){
+			$resource = new Zest_Acl_Resource($resource, $this);
 		}
-		$resource = new Zest_Acl_Resource($resource, $this);
-		if($idProperty){
-			$resource->setIdProperty($idProperty);
+		
+		if($resource instanceof Zest_Acl_Resource){
+			$resource->setOptions($options);
 		}
 		
 		if(!$this->has($resource)){
@@ -67,10 +67,9 @@ class Zest_Acl extends Zend_Acl{
 	 * @return Zest_Acl
 	 */
 	public function addRole($role, $parents = null){
-		if($role instanceof Zend_Acl_Role_Interface){
-			$role = $role->getRoleId();
+		if(is_string($role)){
+			$role = new Zest_Acl_Role($role, $this);
 		}
-		$role = new Zest_Acl_Role($role, $this);
 		
 		if(!$this->_getRoleRegistry()->has($role)){
 			parent::addRole($role, $parents);
@@ -83,36 +82,54 @@ class Zest_Acl extends Zend_Acl{
 	 * @param string $operation
 	 * @param string $type
 	 * @param Zend_Acl_Role_Interface|string|array $roles
-	 * @param Zend_Acl_Resource_Interface|string|array $resources
+	 * @param Zend_Acl_Resource_Interface|string|object $resources
 	 * @param string|array $privileges
 	 * @param Zend_Acl_Assert_Interface $assert
 	 * @return Zest_Acl
 	 */
 	public function setRule($operation, $type, $roles = null, $resources = null, $privileges = null, Zend_Acl_Assert_Interface $assert = null){
-		if(is_array($resources)){
-			$privileges = (array) $privileges;
-			
-			foreach($resources as $resourceId => $values){
-				if(is_string($resourceId)){
-					$resource = $this->get($resourceId);
-					
-					$values = (array) $values;
-					foreach($values as $value){
-						$value = $this->_getResourceValue($resource, $value);
-						if($privileges){
-							foreach($privileges as $privilege){
-								parent::setRule($operation, $type, $roles, $resource, $privilege.$value, $assert);
-							}
-						}
-						else{
-							parent::setRule($operation, $type, $roles, $resource, $value, $assert);
-						}
-					}
-					unset($resources[$resourceId]);
+		if(!is_array($resources)) $resources = array($resources);
+		
+		// si les ressources fournis sont directement des objets, on détecte la resource associée pour chaque objet
+		foreach($resources as $key => $object){
+			if(is_object($object) && !($object instanceof Zend_Acl_Resource_Interface)){
+				if($resource = $this->_getResourceFromObject($object)){
+					$resources[$resource->getResourceId()] = $object;
+					unset($resources[$key]);
+				}
+				else{
+					throw new Zest_Acl_Exception(sprintf('Aucune ressource attachée à la classe "%s".', get_class($object)));
 				}
 			}
 		}
-
+		
+		// gère les tableaux de ressource sous la forme array(resourceId => values)
+		$privileges = (array) $privileges;
+		foreach($resources as $resourceId => $values){
+			if(is_string($resourceId)){
+				$resource = $this->get($resourceId);
+				
+				if(!$resource instanceof Zest_Acl_Resource){
+					throw new Zest_Acl_Exception('Pour utiliser la syntaxe array(resourceId => values) la ressource doit être une instance de Zest_Acl_Resource.');
+				}
+				
+				if(!is_array($values)) $values = (array) $values;
+				foreach($values as $value){
+					$value = $this->_getResourceValue($resource, $value);
+					if($privileges){
+						foreach($privileges as $privilege){
+							parent::setRule($operation, $type, $roles, $resource, $privilege.$value, $assert);
+						}
+					}
+					else{
+						parent::setRule($operation, $type, $roles, $resource, $value, $assert);
+					}
+				}
+				unset($resources[$resourceId]);
+			}
+		}
+		
+		// s'il reste des ressources dans le tableau (qui ont été indexées de manière naturelle : sans clef associative)
 		if($resources){
 			parent::setRule($operation, $type, $roles, $resources, $privileges, $assert);
 		}
@@ -127,15 +144,46 @@ class Zest_Acl extends Zend_Acl{
 	 * @return boolean
 	 */
 	public function isAllowed($role = null, $resource = null, $privilege = null){
+		if(is_object($resource) && !($resource instanceof Zend_Acl_Resource_Interface)){
+			$object = $resource;
+			$resource = $this->_getResourceFromObject($object);
+			if(is_null($resource)){
+				throw new Zest_Acl_Exception(sprintf('Aucune ressource attachée à la classe "%s".', get_class($object)));
+			}
+			$resource = array($resource->getResourceId(), $object);
+		}
+		
 		if(is_array($resource)){
-			$resource = array_pad($resource, 2, null);
-			list($resource, $value) = $resource;
+			if(count($resource) != 2){
+				throw new Zest_Acl_Exception('Le paramètre $resource doit contenir deux cases s\'il est utilisé comme un tableau (resourceId, value).');
+			}
 			
+			list($resource, $value) = $resource;
 			$resource = $this->get($resource);
+			
+			if(!$resource instanceof Zest_Acl_Resource){
+				throw new Zest_Acl_Exception('Pour utiliser la syntaxe array(resourceId, value) la ressource doit être une instance de Zest_Acl_Resource.');
+			}
 			$privilege .= $this->_getResourceValue($resource, $value);
 		}
 		
 		return parent::isAllowed($role, $resource, $privilege);
+	}
+	
+	/**
+	 * @param object $object
+	 * @return Zest_Acl_Resource
+	 */
+	protected function _getResourceFromObject($object){
+		foreach($this->_resources as $resource){
+			$resource = $resource['instance'];
+			if($resource instanceof Zest_Acl_Resource && $className = $resource->getClassName()){
+				if($object instanceof $className){
+					return $resource;
+				}
+			}
+		}
+		return null;
 	}
 	
 	/**
@@ -145,8 +193,8 @@ class Zest_Acl extends Zend_Acl{
 	 */
 	protected function _getResourceValue(Zest_Acl_Resource $resource, $value){
 		if(is_object($value) || is_array($value)){
-			if(!$property = $resource->getIdProperty()){
-				throw new Zest_Acl_Exception(sprintf('Aucune propriété identifiante défini sur la ressource "%s".', $resource->getResourceId()));
+			if(!$property = $resource->getPrimaryAttribute()){
+				throw new Zest_Acl_Exception(sprintf('L\'attribut "primary_attribute" sur la ressource "%s" n\'est pas défini.', $resource->getResourceId()));
 			}
 			
 			// clef public
