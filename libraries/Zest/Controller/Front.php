5161b2cf15ab98e053d129809e72e32f8fe951fe
zest-php <contact@zest-php.com>
2011-02-12 09:00:33 +0100

    ajout de la méthode getModuleUrl sur le frontController

--- libraries/Zest/Controller/Front.php
+++ libraries/Zest/Controller/Front.php
@@ -25,6 +25,39 @@ class Zest_Controller_Front extends Zend_Controller_Front{
 	}
 	
 	/**
+	 * @param string $module
+	 * @return string
+	 */
+	public function getModuleUrl($module){
+		$source = dirname($this->getRequest()->getServer('SCRIPT_FILENAME'));
+		$destination = dirname($this->getModuleDirectory('gui'));
+		return rtrim($this->getRequest()->getBaseUrl(), '/').'/'.$this->_getRelativeDir($source, $destination);
+	}
+	
+	/**
+	 * @param string $sourceDir
+	 * @param string $destinationDir
+	 * @return string
+	 */
+	protected function _getRelativeDir($sourceDir, $destinationDir){
+		$source = str_replace(DIRECTORY_SEPARATOR, '/', $sourceDir);
+		$destination = str_replace(DIRECTORY_SEPARATOR, '/', $destinationDir);
+				
+		$source = explode('/', trim($source, '/'));
+		$destination = explode('/', trim($destination, '/'));
+		
+		foreach($source as $key => $part){
+			if($part === $destination[$key]){
+				unset($source[$key], $destination[$key]);
+			}
+			else{
+				break;
+			}
+		}
+		return str_repeat('../', count($source)).implode('/', $destination);
+	}
+	
+	/**
 	 * @return Zend_Controller_Router_Interface
 	 */
 	public function getRouter(){
