d09064cf973e7995bd63a855e91733f540ca2d8f
zest-php <aimerick@zest-php.com>
2011-04-05 05:45:00 +0200

    mise au propre du filtre de fichier rename

--- libraries/Zest/Filter/File/Rename.php
+++ libraries/Zest/Filter/File/Rename.php
@@ -28,12 +28,13 @@ class Zest_Filter_File_Rename extends Zend_Filter_File_Rename{
 			$newName = parent::getNewName($value, $source);
 		}
 		catch(Zend_Filter_Exception $e){
-			$file = $this->_getFileName($value);
-			if(!is_array($file)){
+			$fileArray = $this->_getFileName($value);
+			if(!is_array($fileArray)){
 				throw $e;
 			}
-			$file = new Zest_File($file['target']);
-			$newName = $file->getPathnameAlternative();
+			$file = new Zest_File($fileArray['source']);
+			$file->rename($fileArray['target'], Zest_File::RENAME_ALTERNATIVE);
+			$newName = $file->getPathname();
 		}
 		return $newName;
 	}
